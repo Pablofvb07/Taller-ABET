950a1c6b30d172c37888d1a47d77563c9d2465af
Pablofvb07 <pablovargas44@gmail.com>
2023-11-26 20:24:41 -0500

    Edicion de codigo finalizado

--- Taller Abet.c
+++ Taller Abet.c
@@ -1,106 +1,136 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-void cargar(char articulos[40][31],int cantidad,float precio);
-void imprimir(char articulos[40][31]);
+
 int main()
 {
-    int i, j, a, b, c, d, e, f, n;
-    int productos[n];
-    char nombre[n][31];
-    char nombres[n][31];
-    int codigo;
-    int cantidad;
-    float precio;
-    int opcion;
-    char seguir[3];
-    do
-    {
-        printf("Bienvenido al sistema de inventario\n");
-        printf("Opciones\n");
-        printf("1. Ingresar un producto nuevo\n");
-        printf("2. Editar un producto\n");
-        printf("3. Eliminar un producto\n");
-        printf("4. Ver lista de productos\n");
-        printf("Ingrese la opción: ");
-        scanf("%d", &opcion);
-        switch (opcion)
-        {
-        case 1:
-        {
-          cargar(nombre,cantidad,precio);
-          imprimir(nombre);
-          getchar();
+  int i, n;
+  char codigos[3][10];
+  float precios[3];
+  char descripciones[3][50];
+  int opcion;
+  char seguir;
 
-            break;}
-        case 2:
-            printf("Ingrese el codigo del producto que desea modificar:\n");
-            scanf("%d", &n);
-            for (i = 0; i < n; i++)
-            {
-                printf("introduce el nombre del producto");
-                printf("Ingrese la cantidad del producto : ");
-                scanf("%d", &cantidad);
-                printf("Ingrese el precio del producto : ");
-                scanf("%f", &precio);
-                printf("Desea realizar alguna otra accion?");
-                scanf("%s", seguir);
-            }
-            break;
-          case 3:
-            printf("Introduce el codigo del producto que deseas eliminar");
-            scanf("%d", &codigo);
-
-          case 4:
-            printf("-------------------------INVENTARIO--------------------------\n");
-              printf("Nombre.\t\t\t Codigo.\t\t Cantidad.\t\t\t Precio.\t\n");
-        default:
-            break;
-
-          }
-    printf("Desea realizar alguna otra accion?");
-    scanf("%s", seguir);}
-    while (strcmp(seguir, "si") == 0 || strcmp(seguir, "SI") == 0) ;
-
-        printf("Gracias por usar el sistema de inventario de ABET :)");
-
-}    
-
-
-void cargar(char articulos[40][31],int cantidad,float precio)
-{
-    int d;
-    int o;
-    int m;
-  float i;
-    
-    printf("Ingrese el numero de  productos que desea ingresar:\n");
-    scanf("%d", &o);
+  do
+  {
+    printf("Opciones\n");
+    printf("1. Ingresar un producto nuevo\n");
+    printf("2. Editar un producto\n");
+    printf("3. Eliminar un producto\n");
+    printf("Ingrese la opción: ");
+    scanf("%d", &opcion);
     getchar();
-    for(d=0;d<o;d++)
+    switch (opcion)
     {
-      
-        printf("Ingrese el nombre del articulo:");
-        fgets(articulos[d],31,stdin);
-        printf("Ingrese la cantidad del producto : ");
-        scanf("%d", &m);
-        printf("Ingrese el precio del producto :");
-        scanf("%f", &i);
-        printf("%s\n",articulos[d]);
-        getchar();
-    }
-}
+    case 1:
+      printf("Ingresar productos\n");
+      printf("Ingrese el numero de productos a ingresar: \n");
+      scanf("%d", &n);
+      for (i = 0; i < n; i++)
+      {
+        printf("Ingrese el código del producto %d: ", i + 1);
+        scanf("%s", codigos[i]);
 
-void imprimir(char articulos[40][31])
-{
-    int d;
-    int o;
-    printf("Ingrese el numero de cantidad de productos ingresados:\n");
-    scanf("%d", &o);
-  printf("-------------------------INVENTARIO--------------------------\n");
-    printf("Nombre.\t\t\t Codigo.\t\t Cantidad.\t\t\t Precio.\t\n");
-    for(d=0;d<o;d++)
+        printf("Ingrese el precio del producto %d: ", i + 1);
+        scanf("%f", &precios[i]);
+
+        printf("Ingrese el nombre del producto %d: ", i + 1);
+        scanf("%s", descripciones[i]);
+        getchar();
+      }
+
+      printf("Datos de productos ingresados:\n");
+      for (i = 0; i < n; i++)
+      {
+        printf("Producto %d:\n", i + 1);
+        printf("Código: %s\n", codigos[i]);
+        printf("Precio: %.2f\n", precios[i]);
+        printf("Nombre: %s\n", descripciones[i]);
+        printf("\n");
+      }
+      break;
+
+    case 2:
+
+      char codigoModificar[10];
+      printf("Ingrese el código del producto que desea modificar: ");
+      scanf("%s", codigoModificar);
+
+      for (i = 0; i < 3; i++)
+      {
+        if (strcmp(codigos[i], codigoModificar) == 0)
+        {
+          printf("Ingrese el nuevo precio para el producto %s: ", codigoModificar);
+          scanf("%f", &precios[i]);
+
+          printf("Datos del producto actualizados:\n");
+          printf("Código: %s\n", codigos[i]);
+          printf("Nuevo Precio: %.2f\n", precios[i]);
+          printf("Nombre: %s\n", descripciones[i]);
+
+          break;
+        }
+      }
+
+      if (i == 3)
+      {
+
+        printf("Producto con código %s no encontrado.\n", codigoModificar);
+      }
+      break;
+
+    case 3:
+  printf("Eliminar productos\n");
+  char codigoeliminar[10];
+  printf("Ingrese el código del producto que desea eliminar: ");
+  scanf("%s", codigoeliminar);
+
+  for (i = 0; i < 3; i++)
+  {
+    if (strcmp(codigos[i], codigoeliminar) == 0)
     {
-        printf("%s,\n",articulos[d]);
+      printf("Producto eliminado correctamente:\n");
+      printf("Código: %s\n", codigos[i]);
+      printf("Precio: %.2f\n", precios[i]);
+      printf("Nombre: %s\n", descripciones[i]);
+
+      // Eliminar el producto moviendo los elementos en el arreglo
+      for (int j = i; j < n - 1; j++)
+      {
+        strcpy(codigos[j], codigos[j + 1]);
+        precios[j] = precios[j + 1];
+        strcpy(descripciones[j], descripciones[j + 1]);
+      }
+
+      // Decrementar el contador de productos
+      n--;
+
+      break;
     }
+  }
+
+  if (i == 3)
+  {
+    printf("Producto con código %s no encontrado.\n", codigoeliminar);
+  }
+  break;
+
+
+    default:
+      printf("Opción no válida\n");
+    }
+
+    printf("¿Desea continuar? (y/n): ");
+    scanf(" %c", &seguir);
+  } while (seguir == 'y' || seguir == 'Y');
+
+  for (i = 0; i < n; i++)
+  {
+    printf("Producto %d:\n", i + 1);
+    printf("Código: %s\n", codigos[i]);
+    printf("Precio: %.2f\n", precios[i]);
+    printf("Descripción: %s\n", descripciones[i]);
+    printf("\n");
+  }
+
+  return 0;
 }
